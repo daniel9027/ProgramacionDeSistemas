4928e24d28b4ceb170d69d2a05fe1f7e1bba2e83
alessandro03 <alessandro6293@gmail.com>
2016-09-07 19:08:54 -0500

    Se agregan comentarios
    Se comentaron todos los metodos de la clase CalculatorVisitor

--- Calculator/Calculator/CalculatorVisitor.cs
+++ Calculator/Calculator/CalculatorVisitor.cs
@@ -13,50 +13,23 @@ namespace Calculator
         {
             memory = new Dictionary<string, double>();
         }
-        /*public override double VisitDouble(CalculatorParser.DoubleContext context)
-        {
-            return double.Parse(context.DOUBLE().GetText());
-        }
-
-        public override double VisitAddSub(CalculatorParser.AddSubContext context)
-        {
-            double left = Visit(context.expr(0));
-            double right = Visit(context.expr(1));
-            if (context.op.Type == CalculatorParser.ADD)
-            {
-                return left + right;
-            }
-            else
-            {
-                return left - right;
-            }
-        }
-
-        public override double VisitMulDiv(CalculatorParser.MulDivContext context)
-        {
-            double left = Visit(context.expr(0));
-            double right = Visit(context.expr(1));
-            if (context.op.Type == CalculatorParser.MUL)
-            {
-                return left * right;
-            }
-            else
-            {
-                return left / right;
-            }
-        }
-
-        public override double VisitParens(CalculatorParser.ParensContext context)
-        {
-            return Visit(context.expr());
-        }*/
 
         #region atom
+        /// <summary>
+        /// Visita el nodo correspondiente a una expresión agrupada por parentesis
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Se devuelve el resultado de la llamada al nodo hijo plusOrMinus</returns>
         public override double VisitBraces(CalculatorParser.BracesContext context)
         {
             return Visit(context.plusOrMinus());
         }
 
+        /// <summary>
+        /// Visita el nodo correspondiente a una variable 
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Se devuelve el valor de la variable</returns>
         public override double VisitVariable(CalculatorParser.VariableContext context)
         {
             string id = context.ID().GetText();
@@ -73,21 +46,41 @@ namespace Calculator
             return val;
         }
 
+        /// <summary>
+        /// Visita el nodo correspondiente a un entero
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el valor del entero</returns>
         public override double VisitInt(CalculatorParser.IntContext context)
         {
             return double.Parse(context.INT().GetText());
         }
 
+        /// <summary>
+        /// Visita el nodo correspondiente a un double
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el valor del double</returns>
         public override double VisitDouble(CalculatorParser.DoubleContext context)
         {
             return double.Parse(context.DOUBLE().GetText());
         }
 
+        /// <summary>
+        /// Visita el nodo correspondiente a la constante e
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el valor de la constante</returns>
         public override double VisitConstantE(CalculatorParser.ConstantEContext context)
         {
             return Math.E;
         }
 
+        /// <summary>
+        /// Visita el nodo correspondiente a la constante PI
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el valor de la constante</returns>
         public override double VisitConstantPI(CalculatorParser.ConstantPIContext context)
         {
             return Math.PI;
@@ -95,11 +88,21 @@ namespace Calculator
         #endregion
 
         #region unaryMinus
+        /// <summary>
+        /// Visita el nodo Atom, el cual defina los valores primitivos a utilizar
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el resultado de la llamada al nodo hijo atom</returns>
         public override double VisitToAtom(CalculatorParser.ToAtomContext context)
         {
             return Visit(context.atom());
         }
 
+        /// <summary>
+        /// Visita el nodo correspondiente al menos unario
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>El valor negativo del resultado a la visita de su nodo hijo</returns>
         public override double VisitChangeSign(CalculatorParser.ChangeSignContext context)
         {
             return ( -1.0d * Visit(context.unaryMinus()) );
@@ -107,6 +110,11 @@ namespace Calculator
         #endregion
 
         #region pow
+        /// <summary>
+        /// Visita el nodo correspondiente a la potencia de un número o expresión
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el resultado de la expresión interna elevada a la potencia señalada</returns>
         public override double VisitPower(CalculatorParser.PowerContext context)
         {
             return context.ChildCount == 1 ? 
@@ -150,6 +158,11 @@ namespace Calculator
         #endregion
 
         #region setVar
+        /// <summary>
+        /// Visita el nodo correspondiente a la asignación de variables
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el valor de la variable tras ser asignada</returns>
         public override double VisitSetVariable(CalculatorParser.SetVariableContext context)
         {
             string id = context.ID().GetText();
@@ -166,6 +179,11 @@ namespace Calculator
             return val;
         }
 
+        /// <summary>
+        /// Vista el nodo correspondiente a una ecuación
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el resultado de la ecuación tras la visita al nodo hijo</returns>
         public override double VisitCalculate(CalculatorParser.CalculateContext context)
         {
             return Visit(context.plusOrMinus());
@@ -173,6 +191,11 @@ namespace Calculator
         #endregion
 
         #region input
+        /// <summary>
+        /// Representa cada una de las entradas en la calculadora
+        /// </summary>
+        /// <param name="context">Variable de contexto</param>
+        /// <returns>Devuelve el resultado de la cadena ingresada</returns>
         public override double VisitToSetVar(CalculatorParser.ToSetVarContext context)
         {
             return Visit(context.setVar());
