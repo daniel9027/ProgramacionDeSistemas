3f02e2bac2bde11379371dc27665441193af11d9
alessandro03 <alessandro6293@gmail.com>
2016-11-03 23:01:09 -0600

    Carga de mapa de memoria desde archivo

--- SIC_Sim/SIC_Sim/FormMapa.Designer.cs
+++ SIC_Sim/SIC_Sim/FormMapa.Designer.cs
@@ -32,6 +32,9 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openRegDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lbDirCarga = new System.Windows.Forms.Label();
+            this.lbTamProg = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.mapaDeMemoria)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -41,9 +44,10 @@
             this.mapaDeMemoria.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.mapaDeMemoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.mapaDeMemoria.Location = new System.Drawing.Point(12, 33);
+            this.mapaDeMemoria.Location = new System.Drawing.Point(18, 71);
+            this.mapaDeMemoria.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.mapaDeMemoria.Name = "mapaDeMemoria";
-            this.mapaDeMemoria.Size = new System.Drawing.Size(527, 365);
+            this.mapaDeMemoria.Size = new System.Drawing.Size(790, 354);
             this.mapaDeMemoria.TabIndex = 0;
             // 
             // menuStrip1
@@ -52,7 +56,8 @@
             this.archivoToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(551, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(826, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -61,8 +66,9 @@
             this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.abrirToolStripMenuItem});
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(55, 18);
             this.archivoToolStripMenuItem.Text = "Archivo";
+            this.archivoToolStripMenuItem.Click += new System.EventHandler(this.archivoToolStripMenuItem_Click);
             // 
             // abrirToolStripMenuItem
             // 
@@ -71,14 +77,45 @@
             this.abrirToolStripMenuItem.Text = "Abrir";
             this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
             // 
+            // openRegDialog
+            // 
+            this.openRegDialog.Filter = "SIC OBJ | *.sobj";
+            this.openRegDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openRegDialog_FileOk);
+            // 
+            // lbDirCarga
+            // 
+            this.lbDirCarga.AutoSize = true;
+            this.lbDirCarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDirCarga.Location = new System.Drawing.Point(14, 36);
+            this.lbDirCarga.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbDirCarga.Name = "lbDirCarga";
+            this.lbDirCarga.Size = new System.Drawing.Size(149, 20);
+            this.lbDirCarga.TabIndex = 2;
+            this.lbDirCarga.Text = "Dirección de carga: ";
+            // 
+            // lbTamProg
+            // 
+            this.lbTamProg.AutoSize = true;
+            this.lbTamProg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTamProg.Location = new System.Drawing.Point(280, 36);
+            this.lbTamProg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTamProg.Name = "lbTamProg";
+            this.lbTamProg.Size = new System.Drawing.Size(169, 20);
+            this.lbTamProg.TabIndex = 3;
+            this.lbTamProg.Text = "Tamaño de programa: ";
+            // 
             // FormMapa
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 410);
+            this.ClientSize = new System.Drawing.Size(826, 439);
+            this.Controls.Add(this.lbTamProg);
+            this.Controls.Add(this.lbDirCarga);
             this.Controls.Add(this.mapaDeMemoria);
             this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormMapa";
             this.Text = "FormMapa";
             this.Load += new System.EventHandler(this.FormMapa_Load);
@@ -96,5 +133,8 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openRegDialog;
+        private System.Windows.Forms.Label lbDirCarga;
+        private System.Windows.Forms.Label lbTamProg;
     }
 }
--- SIC_Sim/SIC_Sim/FormMapa.cs
+++ SIC_Sim/SIC_Sim/FormMapa.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,7 +15,8 @@ namespace SIC_Sim
     public partial class FormMapa : Form
     {
         string[,] mapa;
-        int progSize;
+        int progSize, loadAdress;
+        private string regObj;
         public FormMapa()
         {
             InitializeComponent();
@@ -32,7 +35,7 @@ namespace SIC_Sim
 
         private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            openRegDialog.ShowDialog();
         }
 
         private void carga() {
@@ -52,6 +55,61 @@ namespace SIC_Sim
                         mapaDeMemoria[j, i].Value = mapa[i, j];
                 }
             }
+        }
+
+        private bool leeArchivo()
+        {
+            int k;
+            bool success;
+            string[] lines = Regex.Split(regObj, "\r\n");
+
+            if (lines.Length > 2 && Regex.IsMatch(lines[0], "^H") && Regex.IsMatch(lines[lines.Length - 1], "^E"))
+            {
+                loadAdress = int.Parse(lines[0].Substring(7, 6), System.Globalization.NumberStyles.HexNumber);
+                progSize = int.Parse(lines[0].Substring(13, 6), System.Globalization.NumberStyles.HexNumber);
+                mapa = new string[progSize / 16, 16];
+                for (int c = 0; c < regObj.Length; c++)
+                {
+                    k = 0;
+                    if (regObj[c] == 'T')
+                    {
+                        int dir1 = (int.Parse(regObj.Substring(c + 1, 6), System.Globalization.NumberStyles.HexNumber) - loadAdress) / 16;
+                        int dir2 = (int.Parse(regObj.Substring(c + 1, 6), System.Globalization.NumberStyles.HexNumber) - loadAdress) % 16;
+                        int bytes = int.Parse(regObj.Substring(c + 7, 2), System.Globalization.NumberStyles.HexNumber);
+                        c += 9;
+                        for (int i = dir1; i < progSize / 16 && k < bytes; i++)
+                        {
+                            for (int j = dir2 % 16; j < 16 && k < bytes; j++, k += 2)
+                            {
+                                mapa[i, j] = regObj.Substring(c, 2);
+                                c += 2;
+                            }
+                            dir2 = 0;
+                        }
+                    }
+                }
+                success = true;
+            }
+            else
+                success = false;
+            return success;
+        }
+
+        private void openRegDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            regObj = String.Join("\r\n", File.ReadAllLines(openRegDialog.FileName));
+            if (leeArchivo())
+            {
+                carga();
+                lbDirCarga.Text += loadAdress.ToString("X");
+                lbTamProg.Text += progSize.ToString("X");
+            }
+            else
+                MessageBox.Show("Error al leer archivo", "Lectura de archivo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void archivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
         }
     }
 }
