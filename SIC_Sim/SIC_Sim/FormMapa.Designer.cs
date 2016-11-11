1837eae75ce14253bf44e6b9ad3ab048788d0b4e
alessandro03 <alessandro6293@gmail.com>
2016-11-10 19:34:37 -0600

    Implementacion de efectos

--- SIC_Sim/SIC_Sim/FormMapa.Designer.cs
+++ SIC_Sim/SIC_Sim/FormMapa.Designer.cs
@@ -539,6 +539,7 @@
             this.Controls.Add(this.mapaDeMemoria);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
--- SIC_Sim/SIC_Sim/FormMapa.cs
+++ SIC_Sim/SIC_Sim/FormMapa.cs
@@ -175,42 +175,130 @@ namespace SIC_Sim
 
             string info = string.Empty;
             int avance, valorByte;
+            string cod, modo, etiq;
 
             contadorPrograma = int.Parse(textCP.Text, System.Globalization.NumberStyles.HexNumber);
             avance = contadorPrograma - loadAdress;
+            // Deteccion de elementos (Cod.Operacion, Modo, Etiqueta)
+            valorByte = int.Parse(mapaDeMemoria[(avance + 1) % 16, (avance + 1) / 16].Value.ToString(), System.Globalization.NumberStyles.HexNumber);
+            cod = mapaDeMemoria[avance % 16, avance / 16].Value.ToString();
+            modo = (valorByte >= 128 ? "Indexado" : "Directo");
+            etiq = (valorByte >= 128 ? (valorByte - 128).ToString("X") : valorByte.ToString("X")) + mapaDeMemoria[(avance + 2) % 16, (avance + 2) / 16].Value.ToString();
+            // Llenado de text box 
             info = "CP = " + textCP.Text + "\r\n";
-            info += "Instrucción: " + 
+            info += "Instrucción: " +
                 mapaDeMemoria[avance % 16, avance / 16].Value +
                 mapaDeMemoria[(avance + 1) % 16, (avance + 1) / 16].Value +
                 mapaDeMemoria[(avance + 2) % 16, (avance + 2) / 16].Value + "\r\n";
-            // Deteccion de modo
-            valorByte = int.Parse(mapaDeMemoria[(avance + 1) % 16, (avance + 1) / 16].Value.ToString(), System.Globalization.NumberStyles.HexNumber);
-            info += "CodOP = " + mapaDeMemoria[avance % 16, avance / 16].Value + "; ";
-            info += "Modo = " + (valorByte >= 128 ? "Indexado" : "Directo") + "; ";
-            info += "m = " + (valorByte >= 128 ? (valorByte - 128).ToString("X") : valorByte.ToString("X")) + 
-                mapaDeMemoria[(avance + 2) % 16, (avance + 2) / 16].Value.ToString() + "\r\n";
-            info += "Efecto: " + ObtenEfecto(mapaDeMemoria[avance % 16, avance / 16].Value.ToString(), true, "");
-            // Incremento del CP
-            textCP.Text = (contadorPrograma += 3).ToString("X");
+            info += "CodOP = " + cod + "; ";
+            info += "Modo = " + modo + "; ";
+            info += "m = " + etiq + "\r\n";
+            info += "Efecto: " + ObtenEfecto(cod, modo, etiq);
             textInfo.Text += info;
             textInfo.SelectionStart = textInfo.TextLength;
             textInfo.ScrollToCaret();
+            // Incremento del CP
+            textCP.Text = (contadorPrograma += 3).ToString("X");
         }
 
-        private string ObtenEfecto(string codOp, bool isX, string m)
+        private string ObtenEfecto(string codOp, string modo, string etiq)
         {
             string efecto = string.Empty;
 
             switch (codOp)
             {
                 case "18":
-                    return "ADD m \r\n" + "\tA <- (A) + (m...m+2)\r\n";
+                    if(modo == "Directo")
+                        efecto = "ADD m \r\n" + "\tA <- (A) + (m...m+2)\r\n";
+                    else
+                        efecto = "ADD m,X \r\n" + "\tA <- (A) + ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                case "40":
+                    if (modo == "Directo")
+                        efecto = "AND m \r\n" + "\tA ← (A) & (m..m+2)\r\n";
+                    else
+                        efecto = "AND m,X \r\n" + "\tA ← (A) & ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                case "28":
+                    if (modo == "Directo")
+                        efecto = "COMP m \r\n" + "\t(A) : (m..m+2)\r\n";
+                    else
+                        efecto = "COMP m,X \r\n" + "\t(A) : ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                case "24":
+                    if (modo == "Directo")
+                        efecto = "DIV m \r\n" + "\tA ← (A) / (m..m+2)\r\n";
+                    else
+                        efecto = "DIV m,X \r\n" + "\tA ← (A) / ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                case "3C":
+                    if (modo == "Directo")
+                        efecto = "J m \r\n" + "\tCP ← m\r\n";
+                    else
+                        efecto = "J m,X \r\n" + "\tCP ← m + (X)\r\n";
+                    break;
+                case "30":
+                    if (modo == "Directo")
+                        efecto = "JEQ m \r\n" + "\tCP ← m si CC está en =\r\n";
+                    else
+                        efecto = "JEQ m,X \r\n" + "\tCP ← m + (X) si CC está en =\r\n";
+                    break;
+                case "34":
+                    if (modo == "Directo")
+                        efecto = "JGT m \r\n" + "\tCP ← m si CC está en >\r\n";
+                    else
+                        efecto = "JGT m,X \r\n" + "\tCP ← m + (X) si CC está en >\r\n";
+                    break;
+                case "38":
+                    if (modo == "Directo")
+                        efecto = "JLT m \r\n" + "\tCP ← m si CC está en <\r\n";
+                    else
+                        efecto = "JLT m,X \r\n" + "\tCP ← m + (X) si CC está en <\r\n";
+                    break;
+                case "48":
+                    if (modo == "Directo")
+                        efecto = "JSUB m \r\n" + "\tL ← (CP); CP ← m\r\n";
+                    else
+                        efecto = "JSUB m,X \r\n" + "\tL ← (CP); CP ← m + (X)\r\n";
                     break;
                 case "00":
-                    return "LDA m \r\n" + "\tA <- (m...m+2)\r\n";
+                    if (modo == "Directo")
+                        efecto = "LDA m \r\n" + "\tA ← (m..m+2)\r\n";
+                    else
+                        efecto = "LDA m,X \r\n" + "\tA ← ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                case "50":
+                    if (modo == "Directo")
+                        efecto = "LDCH m \r\n" + "\tA [el byte de más a la derecha]  ← (m)\r\n";
+                    else
+                        efecto = "LDCH m,X \r\n" + "\tA [el byte de más a la derecha]  ← (m + (X))\r\n";
+                    break;
+                case "08":
+                    if (modo == "Directo")
+                        efecto = "LDL m \r\n" + "\tL ← (m..m+2)\r\n";
+                    else
+                        efecto = "LDL m,X \r\n" + "\tL ← ((m + (X))...(m + (X)+2))\r\n";
                     break;
                 case "04":
-                    return "LDX m \r\n" + "\tX <- (m...m+2)\r\n";
+                    if (modo == "Directo")
+                        efecto = "LDX m \r\n" + "\tX ← (m..m+2)\r\n";
+                    else
+                        efecto = "LDX m,X \r\n" + "\tX ← ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                case "20":
+                    if (modo == "Directo")
+                        efecto = "MUL m \r\n" + "\tA ← (A) * (m..m+2)\r\n";
+                    else
+                        efecto = "MUL m,X \r\n" + "\tA ← (A) * ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                case "44":
+                    if (modo == "Directo")
+                        efecto = "OR m \r\n" + "\tA ← (A) | (m..m+2)\r\n";
+                    else
+                        efecto = "OR m,X \r\n" + "\tA ← (A) | ((m + (X))...(m + (X)+2))\r\n";
+                    break;
+                default:
+                        efecto = "INSTRUCCIÓN NO VÁLIDA\r\n";
                     break;
             }
             return efecto;
